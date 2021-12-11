d48064d612a10dd0ecd823c8267e3467d8da3d29
Sergo_Kaz <wwe619dx@ukr.net>
2021-12-11 19:00:31 +0200

    finish commit

--- main.js
+++ main.js
@@ -1,1 +1,16 @@
 'use strict'
+
+// ? Зацикли висновок днів тижня таким чином: «День тижня. Хочеш побачити наступний день? » і так до тих пір, поки користувач натискає OK.
+
+const daysOfTheWeek = ['Понеділок', 'Вівторок', 'Середа', 'Четверг', 'Пятниця', 'Субота', 'Неділя'];
+
+finish:
+while (true) {
+    for (const iterator of daysOfTheWeek) {
+        let userChoice = confirm(`${iterator}\nХочеш побачити наступний день?`);
+    
+        if (!userChoice) {
+            break finish;
+        }
+    }
+}
